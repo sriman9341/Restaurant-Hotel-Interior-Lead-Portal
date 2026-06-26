16562768bd6964069036ecdcb535a0c5fd1d37dc
sriman9341 <sriman9341@example.com>
2026-06-26 17:24:06 +0530

    Apply latest updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Restaurant-Hotel-Interior-Lead-Portal/',
   plugins: [react()],
   server: {
     port: 3000,
